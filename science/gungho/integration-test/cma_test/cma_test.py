1980340613654c7b2d94c6cfd9a5cf03987107a5
ericaneininger <enquiries@metoffice.gov.uk>
2024-04-30 09:53:40 +0000

    ticket:51 : davidlee : Migration of tests to NCI
    git-svn-id: https://metomi/svn/lfric_apps.xm/main/trunk@1072 3896877f-8c05-4358-bc88-7947bb4308c7

--- science/gungho/integration-test/cma_test/cma_test.py
+++ science/gungho/integration-test/cma_test/cma_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 ##############################################################################
 # (c) Crown copyright 2017 Met Office. All rights reserved.
