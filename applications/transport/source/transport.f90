0df05d3eecee16a41b3c4a776abe0037abb253a8
Samantha Adams <svadams@users.noreply.github.com>
2026-08-24 10:45:28 +0100

    Revert "remove use of final_comm for XIOS3"
    This reverts commit 46d23af7cc228f9cdf9195531469581e32cbcd4d.

--- applications/transport/source/transport.f90
+++ applications/transport/source/transport.f90
@@ -11,7 +11,7 @@ program transport
   use cli_mod,                 only: parse_command_line
   use constants_mod,           only: i_def, r_def, l_def, str_max_filename
   use driver_collections_mod,  only: init_collections, final_collections
-  use driver_comm_mod,         only: init_comm
+  use driver_comm_mod,         only: init_comm, final_comm
   use driver_config_mod,       only: init_config, final_config
   use driver_log_mod,          only: init_logger, final_logger
   use driver_modeldb_mod,      only: modeldb_type
@@ -83,5 +83,6 @@ program transport
   call final_timing( program_name )
   call final_logger( program_name )
   call final_config()
+  call final_comm( modeldb )
 
 end program transport
