40c08339ac2e57ceb83c4907462be86e58c54d41
Samantha Adams <svadams@users.noreply.github.com>
2026-08-24 10:45:28 +0100

    Revert "remove use of final_comm for XIOS3"
    This reverts commit 46d23af7cc228f9cdf9195531469581e32cbcd4d.

--- applications/lfric2lfric/source/lfric2lfric.F90
+++ applications/lfric2lfric/source/lfric2lfric.F90
@@ -18,7 +18,7 @@ program lfric2lfric
   use constants_mod,          only: precision_real
   use driver_collections_mod, only: init_collections, final_collections
   use driver_config_mod,      only: init_config, final_config
-  use driver_comm_mod,        only: init_comm
+  use driver_comm_mod,        only: init_comm, final_comm
   use driver_log_mod,         only: init_logger, final_logger
   use driver_modeldb_mod,     only: modeldb_type
   use driver_time_mod,        only: init_time, final_time
@@ -97,5 +97,6 @@ program lfric2lfric
   call final_collections()
   call final_logger( program_name )
   call final_config()
+  call final_comm( modeldb )
 
 end program lfric2lfric
